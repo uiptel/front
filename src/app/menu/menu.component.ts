01357dcdb563ce7da945234512f28cc33ec8e877
Aleksandr N. Ryzhov <a.n.ryzhov@gmail.com>
2020-05-23 18:45:13 +0000

    menu section styles refactor

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -4,14 +4,25 @@ import { Scratch } from '../models/scratch';
 @Component({
   selector: 'app-menu',
   template: `
-    <div class="menu">
-      <p class="title _grey _medium">{{'menu.buy_topup_account' | translate}}</p>
-      <form class="container">
-        <div *ngFor="let scratch of scratches" class="box">
-          <app-scratch-card [scratch]="scratch" (select)="onSelect($event)"></app-scratch-card>
+    <section class="sectionMenu">
+      <div class="sectionMenu__content">
+        <div class="sectionMenu__title">
+          <p class="title _grey _medium">{{ 'menu.buy_topup_account' | translate }}</p>
         </div>
-      </form>
-    </div>
+
+        <form class="sectionMenu__form">
+          <div class="sectionMenu__formEl _flex">
+            <div class="_item" *ngFor="let scratch of scratches" >
+              <app-scratch-card [scratch]="scratch" (select)="onSelect($event)">
+              </app-scratch-card>
+            </div>
+          </div>
+          <div class="sectionMenu__formEl">
+            <button class="button _normal _hard">{{ 'menu.buy_topup' | translate }}</button>
+          </div>
+        </form>
+      </div>
+    </section>
   `,
   styleUrls: ['./menu.component.scss'],
 })

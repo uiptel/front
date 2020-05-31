811c775b4c22b9dec8ea81fbef4d65c251478e50
Aleksandr N. Ryzhov <a.n.ryzhov@gmail.com>
2020-05-31 19:59:39 +0000

    flag icons style, nav component update

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,18 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-nav',
   template: `
     <section class="nav">
       <nav class="navbar" role="navigation" aria-label="main navigation">
-        <div class="navbar-menu">
+        <div class="navbar-brand">
+          <div class="navbar-item">
+            <p class="logo-content">UIPTEL</p>
+          </div>
+          <a role="button" (click)="isActive = !isActive" class="navbar-burger"
+            [ngClass]="{'is-active': isActive}" aria-label="menu" aria-expanded="false">
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+          </a>
+        </div>
+        <div class="navbar-menu" [ngClass]="{'is-active': isActive}">
           <div class="navbar-start">
             <a class="navbar-item">{{ 'nav.top_up' | translate }}</a>
             <a class="navbar-item">{{ 'nav.sales_point' | translate }}</a>
             <a class="navbar-item">{{ 'nav.support' | translate }}</a>
           </div>
           <div class="navbar-end">
-            <a class="navbar-item">EN</a>
+            <a class="navbar-item" (click)="toggle.emit(true)">
+              <span class="flag-icon flag-icon-{{flag}}"></span>
+            </a>
           </div>
         </div>
       </nav>
@@ -20,9 +33,11 @@ import { Component, OnInit } from '@angular/core';
   `,
 })
 export class NavComponent implements OnInit {
+  @Input() public readonly flag: string;
+  @Output() public toggle = new EventEmitter<boolean>();
+  public isActive = false;
 
   constructor() { }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void { }
 }
--- src/app/root.component.ts
+++ src/app/root.component.ts
@@ -5,15 +5,21 @@ import config from 'src/config';
 
 const USER_LANG_KEY = 'lang';
 
+interface Lang {
+  lang: string;
+  flag: string;
+}
+
+const langs: Lang[] = [{lang: 'ru-RU', flag: 'ru'}, {lang: 'en-US', flag: 'gb'}];
+
 @Component({
   selector: 'app-root',
   template: `
     <div class="container root">
-      <app-nav></app-nav>
+      <app-nav [flag]="flag" (toggle)="onToggle()"></app-nav>
       <app-menu></app-menu>
     </div>
     `,
-  styleUrls: ['./root.component.scss'],
 })
 export class RootComponent implements OnInit {
   public lang: string;
@@ -25,6 +31,17 @@ export class RootComponent implements OnInit {
     translate.setDefaultLang(defaultLang);
     translate.use(lang);
     debug(`set default language => ${defaultLang}; user language => ${lang}`);
+  }
+
+  get flag(): string {
+    return langs.find(({lang}) => lang === this.lang)?.flag;
+  }
+
+  onToggle() {
+    const next = langs.find(({lang}) => lang !== this.lang);
+    if (next) {
+      this.translate.use(next.lang);
+    }
   }
 
   ngOnInit(): void {

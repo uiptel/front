ac035a6ddd23eb080cfb6a3805ec4f89bf4ff3bf
Aleksandr N. Ryzhov <a.n.ryzhov@gmail.com>
2020-06-03 19:54:40 +0000

    sale points => layout + styles + load data

--- src/app/sale-point/sale-point.component.ts
+++ src/app/sale-point/sale-point.component.ts
@@ -1,17 +1,47 @@
-import { Component, Inject } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { HttpClient, HttpResponse } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+interface ResponseModel<T> {
+  data: T;
+  success: boolean;
+  error?: {
+    code: number,
+    message: string
+  };
+}
 
 @Component({
   selector: 'app-sale-point',
   template: `
-    <section>
-      <div class="title">
-        <p>Sale Points</p>
+    <section class="sectionPoint">
+      <div class="sectionPoint__content">
+        <div class="sectionPoint__title">
+          <p class="title _grey _medium">{{ 'point.sale_points_address' | translate }}</p>
+        </div>
+        <div class="sectionPoint__table">
+          <ul class="unstyled">
+            <li *ngFor="let point of points">
+              {{ point }}
+            </li>
+          </ul>
+        </div>
       </div>
     </section>
   `,
 })
-export class SalePointComponent {
-  constructor() {
+export class SalePointComponent implements OnInit {
+  public points: string[];
+
+  constructor(private readonly http: HttpClient) {
     console.log('sp:: =>');
+  }
+
+  ngOnInit() {
+    this.getPoints().subscribe(data => this.points = data);
+  }
+
+  private getPoints(): Observable<string[]> {
+    return this.http.get<string[]>('assets/data/sale_points.json');
   }
 }

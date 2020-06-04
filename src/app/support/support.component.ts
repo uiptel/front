e0cecfb31a765fc67008843047261112f6109129
Aleksandr N. Ryzhov <a.n.ryzhov@gmail.com>
2020-06-04 19:15:21 +0000

    support ussd commnds,  update styles, svg icons

--- src/app/support/support.component.ts
+++ src/app/support/support.component.ts
@@ -5,6 +5,36 @@ import { Component } from '@angular/core';
   template: `
     <section class="sectionSupport">
       <div class="sectionSupport__content">
+        <div class="sectionSupport__list">
+          <ul class="unstyled">
+            <li>
+              <div>
+                <span class="ussd">* 100 * {{ 'support.scratch_code' | translate }} #</span>
+                <span class="icon icon-phone-invert"></span>
+              </div>
+            </li>
+            <li>
+              <div>
+                <span class="ussd">
+                  * 101 * 2 * {{ 'support.transfer_amount' | translate }} * {{ 'support.destination_number' | translate }} #
+                </span>
+                <span class="icon icon-phone-invert"></span>
+              </div>
+            </li>
+            <li>
+              <div>
+                <span class="ussd">* 101 #</span>
+                <span class="icon icon-phone-invert"></span>
+              </div>
+            </li>
+            <li>
+              <div>
+                <span class="ussd">* 161 #</span>
+                <span class="icon icon-phone-invert"></span>
+              </div>
+            </li>
+          </ul>
+        </div>
       </div>
     </section>
   `,

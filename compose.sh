1288c6a34090ab4ee94bff8c2083af669cac9fd5
Aleksandr N. Ryzhov <a.n.ryzhov@gmail.com>
2020-07-11 22:33:35 +0300

    update compose.sh

--- compose.sh
+++ compose.sh
@@ -5,10 +5,5 @@
 
 export USER_NAME=`id -u -n` USER_UID=`id -u` APP_VERSION=$(app_version)
 
-if [ -z "$1" ]; then
-	echo "required param for docker-compose missed allow => {build|up|...}"
-	exit 1
-fi
-
 echo "application => ${APP_NAME}, version => ${APP_VERSION}"
 docker-compose ${@}

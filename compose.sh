40f6b97cc2dde155df1bc0b290db5e0050390bdd
Aleksandr N. Ryzhov <a.n.ryzhov@gmail.com>
2020-08-14 12:29:43 +0000

    rm scripts

--- compose.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-. .env
-. .funcs
-
-export USER_NAME=`id -u -n` USER_UID=`id -u` APP_VERSION=$(app_version) APP_NAME=$(app_name)
-
-echo "application => ${APP_NAME}, version => ${APP_VERSION}"
-docker-compose ${@}

a9b071204ed53a1995ccfa80b5a87e070beef25d
Aleksandr N. Ryzhov <a.n.ryzhov@gmail.com>
2020-09-30 19:21:20 +0000

    update docker dev env

--- sleep.js
+++ sleep.js
@@ -1,4 +1,3 @@
-const process = require('process');
 
 const shutdown = message => {
     console.log(message);
@@ -8,5 +7,5 @@ const shutdown = message => {
 process.on('SIGINT',  () => shutdown('Exit on SIGINT'));
 process.on('SIGTERM', () => shutdown('Exit on SIGTERM'));
 
-const sleep = async () => { while(1) await new Promise(resolve => setTimeout(_ => resolve(), 3600000)); };
-sleep();
+const fn = timeout => setTimeout(fn, timeout, timeout);
+fn(3600000);
